37dc57967b6200701281316cbcda8553fa6e07ff
Thanh Nguyen <thanhnt94@gmail.com>
2026-09-17 23:24:36 +0700

    Auto commit from remote update tool - 2026-09-17 23:24:36

--- app/modules/notification/models.py
+++ app/modules/notification/models.py
@@ -13,6 +13,16 @@ class Notification(Base):
     is_read = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
+class PushSubscription(Base):
+    __tablename__ = "push_subscriptions"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, index=True)
+    endpoint = Column(String(512), unique=True, index=True)
+    p256dh = Column(String(255))
+    auth = Column(String(255))
+    created_at = Column(DateTime, default=datetime.utcnow)
+
 class UserTelegramConfig(Base):
     __tablename__ = "user_telegram_configs"
     
